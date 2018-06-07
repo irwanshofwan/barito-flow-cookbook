4b7684bec0fd8d4f49f2512d0bc8f94057fcbc78
Giovanni Sakti <giosakti@gmail.com>
2018-06-07 15:56:07 +0700

    [gio/bherly] also register consumer to consul

--- recipes/consumer_consul_register.rb
+++ recipes/consumer_consul_register.rb
@@ -1,14 +1,14 @@
 #
 # Cookbook:: barito-flow
-# Recipe:: producer_consul_register
+# Recipe:: consumer_consul_register
 #
 # Copyright:: 2018, BaritoLog.
 #
 #
 
 config = {
-  "id": "#{node['hostname']}-#{node[cookbook_name]['producer']['service_name']}",
-  "name": "#{node[cookbook_name]['producer']['service_name']}",
+  "id": "#{node['hostname']}-#{node[cookbook_name]['consumer']['service_name']}",
+  "name": "#{node[cookbook_name]['consumer']['service_name']}",
   "tags": ["app:"],
   "address": node['ipaddress'],
   "port": 8080,
@@ -17,7 +17,7 @@ config = {
   }
 }
 
-consul_register_service "#{node[cookbook_name]['producer']['service_name']}" do
+consul_register_service "#{node[cookbook_name]['consumer']['service_name']}" do
   config config
   config_dir  node[cookbook_name]['consul']['config_dir']
   consul_bin  node[cookbook_name]['consul']['bin']
